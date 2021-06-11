e42b735448c790884c178c49f0f4d1f8fa5fe409
Alexis Analuisa <alexis_analuisa1998@hotmail.com>
2021-06-11 12:09:44 -0500

    iniciando el tema

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="flex items-center space-x-10">
+        <div div class="w-1/12">
+            <img class="rounded-full" src="<?php echo get_template_directory_uri()?>/asset/img/pp.jpg">
+        </div>
+        <div class="w-6/12">
+            <p>Me llamo Alexis Analuisa,tengo 21 años,estudio la carrera de Análisis de sistema en Instituto Tecnologico
+                Quito,la materi que me gusta es programacion en netbens.</p>
+
+            <p>mi hobbit son los video juegos ,el juego que me encanta es Valorant</p>
+            <div div class="w-1/12">
+                <img class="rounded-full" src="<?php echo get_template_directory_uri() ?>/asset/img/V.jpeg">
+            </div>
+            <p>Tuve la oportunida de realizar mis pasantias en la Agencia Nacional de transito, donde puede
+            aplicar mis conocimientos de programcion.</p>
+        </div>
+        
+   <div>
+  <p>
+  Gracias al Instito Tecnologico Quito en la practicas de vinculacion realice una pagina
+  web para una fundacion, el proyecto me fue super bien conoci a gente que me apoyo con cursos de desarrollo web 
+  y ilustrador para realizar revistas digitales .
+  </p>
+   </div>
+
+    </div>
+</main>
+
+<?php get_footer(); ?>
+    
+
